404b13175a92255a8a6776a2154765ecc95da18f
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2018-10-25 00:32:23 +0200

    project init

---
+++ spec/planificador_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Planificador do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/planificador.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Planificador`
+module Planificador
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
